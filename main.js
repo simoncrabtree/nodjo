3dcc5279fadac533701c8f2477add461148438cf
Simon Crabtree <simon@simoncrabtree.net>
2012-08-17 17:16:59 +0100

    Added a simple index view

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var express = require('express');
+
 exports.startServer = function () {
-    console.log("Starting Server");
+    console.log("Starting Server!");
+
+    var app = express();
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+
+    app.get('/', function (req, res) {
+        res.render("index");
+    });
+
+    app.listen(3000);
+    console.log("Server listening on port 3000");
 }
