396acea91f7af47dd67e8d1c73da099d76796d33
Simon Crabtree <simon@simoncrabtree.net>
2012-08-24 20:21:53 +0100

    Added specrunner and a /runspecs route

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 var express = require('express');
 
-exports.startServer = function () {
-    console.log("Starting Server!");
+exports.startServer = function (options) {
 
     var app = express();
     app.set('view engine', 'ejs');
-    app.set('views', process.env.PWD + '/server/views');
     app.use(express.static(process.env.PWD + '/client'));
 
+    app.configure('development', function () {
+        console.log("Running in Development Mode");
+        app.get('/runspecs', function (req, res) {
+            app.set('views', __dirname + '/specrunner');
+            res.render("specrunner");
+        });
+    });
+
     app.get('/', function (req, res) {
+        app.set('views', process.env.PWD + '/server/views');
         res.render("index");
     });
 
-    app.listen(3000);
-    console.log("Server listening on port 3000");
+    app.listen(options.port);
+    console.log("Server listening on port", options.port);
 }
