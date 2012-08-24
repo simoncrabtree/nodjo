b7c9fd91aa9cd44cf09aeb5b5820b9fb7fe7206d
Simon Crabtree <simon@simoncrabtree.net>
2012-08-24 13:40:30 +0100

    Added very simple client-side code which uses dojo (from a CDN) to the index view

---
+++ client/app/main.js
@@ -0,0 +1,7 @@
+define(function () {
+    return {
+        start: function () {
+            console.log("Dojo Ready!");
+        }
+    };
+});
---
+++ client/dojoconfig.js
@@ -0,0 +1,5 @@
+dojoConfig = {
+    packages: [{
+        name: 'app', location: '/app'
+    }]
+};
